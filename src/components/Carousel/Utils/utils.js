472f8a4c15f1d39fd361c0cc268e81b40a34c6d3
Mkhue <mkhue47@gmail.com>
2024-03-20 15:22:42 +0200

    adjusted items shown per carousel

--- src/components/Carousel/Utils/utils.js
+++ src/components/Carousel/Utils/utils.js
@@ -16,25 +16,19 @@ export const carouselList = (numberOfItemsShown, dataList) => {
 export const itemsShownPerScreenSize = (screenSize) => {
   if (screenSize > 1940) {
     return 6;
-  } if (screenSize <= 1940 && screenSize > 1585) {
+  } else if (screenSize > 1585) {
     return 5;
-  } if (screenSize <= 1585 && screenSize > 1350) {
+  } else if (screenSize > 1350) {
     return 4;
-  } if (screenSize <= 1350 && screenSize > 1150) {
+  } else if (screenSize > 1150) {
     return 3;
-  } if (screenSize <= 1150 && screenSize > 980) {
+  } else if (screenSize > 980) {
     return 2;
-  } if (screenSize <= 980 && screenSize > 935) {
+  } else if (screenSize > 760) {
     return 4;
-  } if (screenSize <= 935 && screenSize > 760) {
-    return 4;
-  }if (screenSize <= 760 && screenSize > 570) {
+  } else if (screenSize > 570) {
     return 3;
-  }
-if (screenSize <= 570 && screenSize > 400) {
-  return 2;
-}
-  if (screenSize <= 400) {
-    return 1;
-  }
+  } else if (screenSize > 400) {
+    return 2;
+  } else return 1;
 };
